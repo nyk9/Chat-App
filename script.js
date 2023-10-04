7201f9deefe483b1fa258937764d7d639e4450e4
key-books <eijikagioto@gmail.com>
2023-10-04 16:05:41 +0900

    見づらくなってしまったが、全く同じ処理

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 /**
  * @key-books
+ * @return {value}
  */
 const output_chat = () => {
     //入力したものの取得
     let chat = document.querySelector("#text");
-    console.log(chat);
-    chat.addEventListener('input', function(){
-        if(chat == null){
+    let sub = document.getElementById("form2");
+    sub.addEventListener('submit', ()=>{
+        if(sub == null){
             return false;
         }
-           // 出力
-        let output = document.querySelector("#form1");
-        output.innerHTML = chat.value;
+        console.log(chat.value);
+        // 出力
+        let output = document.querySelector(".output_chat");
+        output.innerHTML = "<div class='output_chat'><p>"+chat.value+"</p></div>";
+        // return chat.value;
     })
 
 };
 output_chat();
+// // 出力
+// let output = document.querySelector("#form1");
+// output.innerHTML = c;
