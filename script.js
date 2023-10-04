74cf65c485cb6ad6ed5f0e4d7779855bc5424f26
key-books <eijikagioto@gmail.com>
2023-10-04 15:01:13 +0900

    入力したものを正しく出力できた addEventListenerを初めて使った

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 /**
  * @key-books
  */
-const chat = () =>{
-    console.log("chat");
-    document.write("chat");
+const output_chat = () => {
+    //入力したものの取得
+    let chat = document.querySelector("#text");
+    console.log(chat);
+    chat.addEventListener('input', function(){
+        if(chat == null){
+            return false;
+        }
+           // 出力
+        let output = document.querySelector("#form1");
+        output.innerHTML = chat.value;
+    })
+
 };
+output_chat();
