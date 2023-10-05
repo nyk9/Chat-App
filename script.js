c75f876087b5bd77c3de85eb43dccf1d5f414859
key-books <eijikagioto@gmail.com>
2023-10-05 14:43:03 +0900

    更に見やすくした

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 //入力したものの取得
-let chat = document.querySelector("#text");
-let sub = document.getElementById("form2");
-sub.addEventListener('submit', ()=>{
-    if(sub == null){
-        return false;
-    }
-    console.log(chat.value);
-    // 出力
-    let output = document.querySelector(".output_chat");
-    output.innerHTML = "<div class='output_chat'><p>"+chat.value+"</p></div>";
-    // return chat.value;
-});
+window.onload = function(){
+    let chat = document.querySelector("#text");
+    let sub = document.getElementById("input_chat");
+    sub.addEventListener('click', ()=>{
+        if(sub == null){
+            return false;
+        }
+        console.log(chat.value);
+        // 出力
+        let output = document.querySelector("#output_chat");
+        output.innerHTML += `<p class='output_chat'>${chat.value}</p>`;
+        chat.value = "";
+    });
+};
