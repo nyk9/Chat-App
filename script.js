12d750040b609acf35552929e95797305bbda395
key-books <eijikagioto@gmail.com>
2023-10-05 14:15:04 +0900

    イベントリスナの簡略化

--- script.js
+++ script.js
@@ -1,24 +1,13 @@
-/**
- * @key-books
- * @return {value}
- */
-const output_chat = () => {
-    //入力したものの取得
-    let chat = document.querySelector("#text");
-    let sub = document.getElementById("form2");
-    sub.addEventListener('submit', ()=>{
-        if(sub == null){
-            return false;
-        }
-        console.log(chat.value);
-        // 出力
-        let output = document.querySelector(".output_chat");
-        output.innerHTML = "<div class='output_chat'><p>"+chat.value+"</p></div>";
-        // return chat.value;
-    })
-
-};
-output_chat();
-// // 出力
-// let output = document.querySelector("#form1");
-// output.innerHTML = c;
+//入力したものの取得
+let chat = document.querySelector("#text");
+let sub = document.getElementById("form2");
+sub.addEventListener('submit', ()=>{
+    if(sub == null){
+        return false;
+    }
+    console.log(chat.value);
+    // 出力
+    let output = document.querySelector(".output_chat");
+    output.innerHTML = "<div class='output_chat'><p>"+chat.value+"</p></div>";
+    // return chat.value;
+});
