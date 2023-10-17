80ae968a4da3402ff3a023df2f92d37a0764c263
key-books <eijikagioto@gmail.com>
2023-10-17 10:33:14 +0900

    ajaxを追加した

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
-//入力したものの取得
-// window.onload = function(){
-//     let chat = document.querySelector("#text");
-//     let sub = document.getElementById("input_chat");
-//     sub.addEventListener('click', ()=>{
-//         if(chat.value == ""){
-//             return false;
-//         }
-//         console.log(chat.value);
-//         // 出力
-//         let output = document.querySelector("#output_chat");
-//         output.innerHTML += `<div class='output_chat'><p>${chat.value}</p><div>`;
-//         chat.value = "";
-//     });
-// };
+/**
+ * @key-books
+ *
+ */
+$.ajax({
+    type: "method",
+    url: "url",
+    data: "data",
+    dataType: "dataType",
+    success: function (response) {
+        
+    }
+});
 
 /**
  * @key-books
